494ecb0aa8a86595fead48a4c3bb3bd690c66060
fukuta <>
2024-11-19 08:27:32 +0900

    Create simulator base class

--- include/n_body_class.cuh
+++ include/n_body_class.cuh
@@ -1,17 +1,17 @@
 #include <memory>
 
-namespace Simulator{
+namespace SimulatorBase{
 
 using FloatArray1D = std::unique_ptr<float[]>;
 using FloatArray3D = std::unique_ptr<float[]>[3];
 
 class NBodySimulator {
-public:
-    void initialize();
-    void evolve_single_step();
-    void show_total_energy();
-    void dump_coordinate();
-    void ending();
+protected:
+    virtual void initialize(int, float) = 0;
+    //virtual void evolve_single_step() = 0;
+    //virtual void show_total_energy() const = 0;
+    //virtual void dump_coordinate() const = 0;
+    //virtual void ending() = 0;
 private:
     int N;
     float dt;

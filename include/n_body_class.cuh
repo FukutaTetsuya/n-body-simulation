69894bcb2daead6cb07b503e9069492e644b12d5
fukuta <>
2024-11-18 21:43:32 +0900

    Make class template

--- include/n_body_class.cuh
+++ include/n_body_class.cuh
@@ -1,12 +1,23 @@
-#include<stdio.h>
+#include <memory>
 
 namespace Simulator{
+
+using FloatArray1D = std::unique_ptr<float[]>;
+using FloatArray3D = std::unique_ptr<float[]>[3];
+
 class NBodySimulator {
 public:
-    void function();
+    void initialize();
+    void evolve_single_step();
+    void show_total_energy();
+    void dump_coordinate();
+    void ending();
+private:
+    int N;
+    float dt;
+    FloatArray1D mass;
+    FloatArray3D r;
+    FloatArray3D v;
+    FloatArray3D a;
 };
-void NBodySimulator::function() {
-    printf("hello\n");
-    return; 
-}
 }

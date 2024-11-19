9a426c5c6f5830efac512c683b48479fe8100aa9
fukuta <>
2024-11-19 18:21:12 +0900

    Impl CPU simulation outline in main

--- main.cu
+++ main.cu
@@ -1,7 +1,19 @@
-#include"n_body_class.cuh"
+#include"n_body_cpu_class.cuh"
+#include<string>
 
 int main(){
-    auto sim = Simulator::NBodySimulator();
-    sim.function();
+    auto sim = SimulatorCPU::NBodySimulatorCPU();
+    const unsigned int N = 32;
+    const float dt = 0.01;
+    float t = 0.0;
+    const std::string output_file_name = "history.txt";
+    sim.initialize(N, dt, output_file_name);
+
+    sim.dump_coordinate();
+    for(unsigned int step = 0; step < 10; step++) {
+        t += dt;
+        sim.evolve_single_step();
+    }
+    sim.ending();
     return 0;
 }
